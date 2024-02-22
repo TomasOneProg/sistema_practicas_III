e0e0009bfb081965a64c8f16877c005435526ad0
TomasOneProg <tomasborstner@gmail.com>
2024-02-22 13:55:44 -0300

    Actualizacion

--- CapaVistaUsuario/Administrador/frmStock.Designer.cs
+++ CapaVistaUsuario/Administrador/frmStock.Designer.cs
@@ -40,13 +40,13 @@ namespace CapaVistaUsuario
             this.btnGuardar = new System.Windows.Forms.Button();
             this.dgvStock = new System.Windows.Forms.DataGridView();
             this.grpStock = new System.Windows.Forms.GroupBox();
+            this.dtpFechaVencimiento = new System.Windows.Forms.DateTimePicker();
             this.lblPrecioTotal = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.txtCantidad = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.txtNumeroLote = new System.Windows.Forms.TextBox();
-            this.dtpFechaVencimiento = new System.Windows.Forms.DateTimePicker();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvStock)).BeginInit();
             this.grpStock.SuspendLayout();
@@ -62,18 +62,16 @@ namespace CapaVistaUsuario
             this.panel1.Controls.Add(this.btnCancelar);
             this.panel1.Controls.Add(this.btnGuardaCambios);
             this.panel1.Controls.Add(this.btnGuardar);
-            this.panel1.Location = new System.Drawing.Point(2, -27);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Location = new System.Drawing.Point(1, -18);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(190, 692);
+            this.panel1.Size = new System.Drawing.Size(127, 450);
             this.panel1.TabIndex = 7;
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(15, 633);
-            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSalir.Location = new System.Drawing.Point(10, 411);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(159, 35);
+            this.btnSalir.Size = new System.Drawing.Size(106, 23);
             this.btnSalir.TabIndex = 6;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -81,10 +79,9 @@ namespace CapaVistaUsuario
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(15, 132);
-            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEliminar.Location = new System.Drawing.Point(10, 86);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(159, 35);
+            this.btnEliminar.Size = new System.Drawing.Size(106, 23);
             this.btnEliminar.TabIndex = 5;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
@@ -92,10 +89,9 @@ namespace CapaVistaUsuario
             // 
             // btnModificar
             // 
-            this.btnModificar.Location = new System.Drawing.Point(15, 83);
-            this.btnModificar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnModificar.Location = new System.Drawing.Point(10, 54);
             this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(159, 35);
+            this.btnModificar.Size = new System.Drawing.Size(106, 23);
             this.btnModificar.TabIndex = 3;
             this.btnModificar.Text = "Modificar";
             this.btnModificar.UseVisualStyleBackColor = true;
@@ -103,10 +99,9 @@ namespace CapaVistaUsuario
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(15, 38);
-            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAgregar.Location = new System.Drawing.Point(10, 25);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(159, 35);
+            this.btnAgregar.Size = new System.Drawing.Size(106, 23);
             this.btnAgregar.TabIndex = 0;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
@@ -114,10 +109,9 @@ namespace CapaVistaUsuario
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(15, 131);
-            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCancelar.Location = new System.Drawing.Point(10, 85);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(159, 35);
+            this.btnCancelar.Size = new System.Drawing.Size(106, 23);
             this.btnCancelar.TabIndex = 2;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
@@ -125,10 +119,9 @@ namespace CapaVistaUsuario
             // 
             // btnGuardaCambios
             // 
-            this.btnGuardaCambios.Location = new System.Drawing.Point(15, 83);
-            this.btnGuardaCambios.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGuardaCambios.Location = new System.Drawing.Point(10, 54);
             this.btnGuardaCambios.Name = "btnGuardaCambios";
-            this.btnGuardaCambios.Size = new System.Drawing.Size(159, 35);
+            this.btnGuardaCambios.Size = new System.Drawing.Size(106, 23);
             this.btnGuardaCambios.TabIndex = 4;
             this.btnGuardaCambios.Text = "Guardar Cambios";
             this.btnGuardaCambios.UseVisualStyleBackColor = true;
@@ -136,10 +129,9 @@ namespace CapaVistaUsuario
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(15, 38);
-            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGuardar.Location = new System.Drawing.Point(10, 25);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(159, 35);
+            this.btnGuardar.Size = new System.Drawing.Size(106, 23);
             this.btnGuardar.TabIndex = 7;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
@@ -148,11 +140,10 @@ namespace CapaVistaUsuario
             // dgvStock
             // 
             this.dgvStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvStock.Location = new System.Drawing.Point(196, 170);
-            this.dgvStock.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvStock.Location = new System.Drawing.Point(131, 110);
             this.dgvStock.Name = "dgvStock";
             this.dgvStock.RowHeadersWidth = 62;
-            this.dgvStock.Size = new System.Drawing.Size(695, 490);
+            this.dgvStock.Size = new System.Drawing.Size(463, 318);
             this.dgvStock.TabIndex = 6;
             this.dgvStock.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStock_RowEnter);
             // 
@@ -168,86 +159,80 @@ namespace CapaVistaUsuario
             this.grpStock.Controls.Add(this.txtNumeroLote);
             this.grpStock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.grpStock.ForeColor = System.Drawing.Color.White;
-            this.grpStock.Location = new System.Drawing.Point(196, 0);
-            this.grpStock.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grpStock.Location = new System.Drawing.Point(131, 0);
             this.grpStock.Name = "grpStock";
-            this.grpStock.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.grpStock.Size = new System.Drawing.Size(695, 163);
+            this.grpStock.Size = new System.Drawing.Size(463, 106);
             this.grpStock.TabIndex = 5;
             this.grpStock.TabStop = false;
             this.grpStock.Text = "groupBox1";
             // 
+            // dtpFechaVencimiento
+            // 
+            this.dtpFechaVencimiento.Location = new System.Drawing.Point(122, 74);
+            this.dtpFechaVencimiento.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtpFechaVencimiento.Name = "dtpFechaVencimiento";
+            this.dtpFechaVencimiento.Size = new System.Drawing.Size(215, 20);
+            this.dtpFechaVencimiento.TabIndex = 35;
+            // 
             // lblPrecioTotal
             // 
             this.lblPrecioTotal.AutoSize = true;
-            this.lblPrecioTotal.Location = new System.Drawing.Point(807, 101);
-            this.lblPrecioTotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPrecioTotal.Location = new System.Drawing.Point(538, 66);
             this.lblPrecioTotal.Name = "lblPrecioTotal";
-            this.lblPrecioTotal.Size = new System.Drawing.Size(0, 20);
+            this.lblPrecioTotal.Size = new System.Drawing.Size(0, 13);
             this.lblPrecioTotal.TabIndex = 33;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(92, 85);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(61, 55);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(73, 20);
+            this.label7.Size = new System.Drawing.Size(49, 13);
             this.label7.TabIndex = 13;
             this.label7.Text = "Cantidad";
             // 
             // txtCantidad
             // 
-            this.txtCantidad.Location = new System.Drawing.Point(183, 79);
-            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCantidad.Location = new System.Drawing.Point(122, 51);
             this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(106, 26);
+            this.txtCantidad.Size = new System.Drawing.Size(72, 20);
             this.txtCantidad.TabIndex = 6;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(8, 119);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(5, 77);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(157, 20);
+            this.label5.Size = new System.Drawing.Size(106, 13);
             this.label5.TabIndex = 9;
             this.label5.Text = "Fecha de nacimiento";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(48, 41);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(11, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.Size = new System.Drawing.Size(105, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Numero de lote";
+            this.label1.Text = "Numero de unidades";
             // 
             // txtNumeroLote
             // 
-            this.txtNumeroLote.Location = new System.Drawing.Point(183, 41);
-            this.txtNumeroLote.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumeroLote.Location = new System.Drawing.Point(122, 27);
             this.txtNumeroLote.Name = "txtNumeroLote";
-            this.txtNumeroLote.Size = new System.Drawing.Size(277, 26);
+            this.txtNumeroLote.Size = new System.Drawing.Size(186, 20);
             this.txtNumeroLote.TabIndex = 0;
             // 
-            // dtpFechaVencimiento
+            // frmStock
             // 
-            this.dtpFechaVencimiento.Location = new System.Drawing.Point(183, 114);
-            this.dtpFechaVencimiento.Name = "dtpFechaVencimiento";
-            this.dtpFechaVencimiento.Size = new System.Drawing.Size(321, 26);
-            this.dtpFechaVencimiento.TabIndex = 35;
-            // 
-            // FrmStock
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(894, 655);
+            this.ClientSize = new System.Drawing.Size(596, 426);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dgvStock);
             this.Controls.Add(this.grpStock);
-            this.Name = "FrmStock";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmStock";
             this.Text = "FrmStock";
             this.Load += new System.EventHandler(this.frmStock_Load);
             this.panel1.ResumeLayout(false);
